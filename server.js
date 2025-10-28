c41098256c06fd70327cdce8b56850d35c8c4144
Zinneerah <zinneerah.rafiq@10pearls.com>
2025-10-28 14:44:23 +0500

    add greet endpoint and button

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+
 app.post('/api/click', (req, res) => {
   res.json({ message: 'Button clicked!' });
+});
+
+app.get('/api/greet', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
 });
 
 app.listen(PORT, () => {
